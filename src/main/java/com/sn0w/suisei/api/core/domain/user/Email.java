db4688440a550a8fcbbe003ef14ac7b9222b543f
abdurrahman ali <abdurr.ali17@gmail.com>
2026-03-14 05:18:32 +0700

    feat:user:domain: add limit str len for email

--- src/main/java/com/sn0w/suisei/api/core/domain/user/Email.java
+++ src/main/java/com/sn0w/suisei/api/core/domain/user/Email.java
@@ -6,6 +6,8 @@ public class Email {
     private static final Pattern REGEXP = Pattern.compile(
             "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,6}$",
             Pattern.CASE_INSENSITIVE);
+    private static final Integer MIN_LEN = 3;
+    private static final Integer MAX_LEN = 255;
 
     private final String value;
 
@@ -16,6 +18,11 @@ public class Email {
 
         if (!REGEXP.matcher(value).matches()) {
             throw new IllegalArgumentException("Invalid email format, expected format: user@example.com");
+        }
+
+        if (value.length() < MIN_LEN || value.length() > MAX_LEN) {
+            throw new IllegalArgumentException("Email must be between " + MIN_LEN +
+                    " and" + MAX_LEN + " characters");
         }
 
         this.value = value;
