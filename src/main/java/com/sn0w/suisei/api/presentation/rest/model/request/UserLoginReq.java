c2eff2b1db4bcacf544be362b80e980099897a22
abdurrahman ali <abdurr.ali17@gmail.com>
2026-03-14 14:54:08 +0700

    feat:user:auth: implement validation for dto/request - input sanitazion purpose

--- src/main/java/com/sn0w/suisei/api/presentation/rest/model/request/UserLoginReq.java
+++ src/main/java/com/sn0w/suisei/api/presentation/rest/model/request/UserLoginReq.java
@@ -1,7 +1,14 @@
 package com.sn0w.suisei.api.presentation.rest.model.request;
 
+import jakarta.validation.constraints.Pattern;
+import org.apache.logging.log4j.core.config.plugins.validation.constraints.NotBlank;
+import org.jspecify.annotations.NonNull;
+
 public record UserLoginReq(
+        @NotBlank(message = "Username or Email is requierd")
         String identifier,
+
+        @NotBlank(message = "Password is requierd")
         String password
 ) {
 }
