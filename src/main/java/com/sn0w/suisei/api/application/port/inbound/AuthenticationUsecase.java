dc6d733237796a8b39984dbd84a51c2124d39023
abdurrahman ali <abdurr.ali17@gmail.com>
2026-03-31 13:24:27 +0700

    feat: add some chages

--- src/main/java/com/sn0w/suisei/api/application/port/inbound/AuthenticationUsecase.java
+++ src/main/java/com/sn0w/suisei/api/application/port/inbound/AuthenticationUsecase.java
@@ -6,7 +6,4 @@ import com.sn0w.suisei.api.core.domain.user.User;
 public interface AuthenticationUsecase {
     void register(UserRegisterCommand registerCommand);
     User login(String identifier, String password);
-    // TODO : create service for check identifier
-    // TODO : create service for check email
-    // TODO : create service for check phone number
 }
